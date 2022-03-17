c2f2e5190dded2943ed117bb64ce31169cb0360a
Elwin <abc602456615@163.com>
2022-03-17 14:17:45 +0000

    add analyse script

---
+++ analyse.py
@@ -0,0 +1,43 @@
+import json
+
+import matplotlib.pyplot as plt
+
+with open('output.json', 'r') as f:
+    result = json.load(f)
+
+# plotting = {}
+indexs = []
+losses = []
+val_losses = []
+accuracies = []
+
+for k, v in result.items():
+    name = k.replace(".pth", "").split('-')
+    index = int(name[0][2:])
+    loss = float(name[1].replace("loss", ""))
+    val_loss = float(name[2].replace("val_loss", ""))
+    accuracy = v["accuracy"]
+    # Add to list
+    indexs.append(index)
+    losses.append(loss)
+    val_losses.append(val_loss)
+    accuracies.append(accuracy)
+    # plotting[index] = {
+    #     "loss": loss,
+    #     "val_loss": val_loss,
+    #     "accuracy":  accuracy,
+    # }
+
+    print(index, loss, val_loss, accuracy)
+
+
+plt.xlabel('Index')
+plt.ylabel('Value')
+# for k, v in plotting.items():
+#     plt.plot(k, v['loss'])
+plt.plot(indexs, losses, label="loss")
+plt.plot(indexs, val_losses, label="validation_loss")
+plt.plot(indexs, accuracies, label="accuracy")
+# print((k, v['loss']) for k, v in plotting.items())
+plt.legend()
+plt.show()
---
+++ test.py
@@ -0,0 +1,72 @@
+import json
+from pathlib import Path
+
+from PIL import Image
+from matplotlib import pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, classification_report
+
+from siamese import Siamese
+
+test_set = Path('./testdata')
+model_path = Path('./logs_cpy')
+
+tests = []
+for animal_name in test_set.glob('*'):
+    # print(f'- {animal_name.name}')
+    for img_path in animal_name.glob('*'):
+        tests.append((img_path.absolute(), animal_name.name))
+
+atest = []
+# Construct test sets and ground truth
+for i in range(len(tests)):
+    for j in range(i + 1, len(tests)):
+        path1, animal1 = tests[i]
+        path2, animal2 = tests[j]
+        atest.append((path1, path2, animal1 == animal2))  # Ground Truth
+
+
+def evaluation(predict, truth):
+    # Confusion Matrix
+    disp = ConfusionMatrixDisplay.from_predictions(truth, predict)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+    plt.show()
+    # Other evaluations
+    report = classification_report(truth, predict, digits=5, output_dict=True)
+    print(
+        f"Classification report for classifier:\n"
+        f"{classification_report(truth, predict, digits=5)}\n"
+    )
+    return report
+
+
+def main(model_pth):
+    model = Siamese(model_path=model_pth)
+    Y = []
+    prediction = []
+    process = 0
+    total = len(atest)
+    for tup in atest:
+        # if process == 1000:
+        #     break
+        p1, p2, res = tup
+        img_1 = Image.open(p1)
+        img_2 = Image.open(p2)
+        Y.append(res)
+        result = model.detect_image(img_1, img_2)
+        prediction.append(True if result > 0.5 else False)
+        process += 1
+        print(f"Processing...({process}/{total})")
+    # print(Y)
+    # print(prediction)
+    return evaluation(prediction, Y)
+
+
+evaluation_feedback = {}
+for i in model_path.glob('*'):
+    print(f'loading model {i}...')
+    evaluation_feedback[i.name] = main(i)
+
+print(evaluation_feedback)
+with open('output.json', 'w+') as f:
+    json.dump(evaluation_feedback, f, indent=4)
