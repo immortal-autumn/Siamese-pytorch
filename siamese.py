fd22635c9c9d4a1c6d7214e7bac0c9ff3e0a6029
Elwin <abc602456615@163.com>
2022-03-17 14:13:24 +0000

    update code

--- siamese.py
+++ siamese.py
@@ -7,25 +7,26 @@ from PIL import Image
 from nets.siamese import Siamese as siamese
 
 
-#---------------------------------------------------#
+# ---------------------------------------------------#
 #   使用自己训练好的模型预测需要修改model_path参数
-#---------------------------------------------------#
+# ---------------------------------------------------#
 class Siamese(object):
     _defaults = {
-        #-----------------------------------------------------#
+        # -----------------------------------------------------#
         #   使用自己训练好的模型进行预测一定要修改model_path
         #   model_path指向logs文件夹下的权值文件
-        #-----------------------------------------------------#
-        "model_path"    : 'model_data/Omniglot_vgg.pth',
-        #-----------------------------------------------------#
+        # -----------------------------------------------------#
+        # "model_path": 'model_data/Omniglot_vgg.pth',
+        "model_path": './logs_cpy/ep054-loss0.026-val_loss0.167.pth',
+        # -----------------------------------------------------#
         #   输入图片的大小。
-        #-----------------------------------------------------#
-        "input_shape"   : (105, 105, 3),
-        #-------------------------------#
+        # -----------------------------------------------------#
+        "input_shape": (105, 105, 3),
+        # -------------------------------#
         #   是否使用Cuda
         #   没有GPU可以设置成False
-        #-------------------------------#
-        "cuda"          : True
+        # -------------------------------#
+        "cuda": True
     }
 
     @classmethod
@@ -35,23 +36,25 @@ class Siamese(object):
         else:
             return "Unrecognized attribute name '" + n + "'"
 
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     #   初始化Siamese
-    #---------------------------------------------------#
-    def __init__(self, **kwargs):
+    # ---------------------------------------------------#
+    def __init__(self, model_path=None, **kwargs):
+        if model_path is not None:
+            self._defaults['model_path'] = model_path
         self.__dict__.update(self._defaults)
         self.generate()
-        
-    #---------------------------------------------------#
+
+    # ---------------------------------------------------#
     #   载入模型
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def generate(self):
-        #---------------------------#
+        # ---------------------------#
         #   载入模型与权值
-        #---------------------------#
+        # ---------------------------#
         print('Loading weights into state dict...')
-        device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model   = siamese(self.input_shape)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = siamese(self.input_shape)
         model.load_state_dict(torch.load(self.model_path, map_location=device))
         self.net = model.eval()
         print('{} model loaded.'.format(self.model_path))
@@ -60,64 +63,64 @@ class Siamese(object):
             self.net = torch.nn.DataParallel(self.net)
             cudnn.benchmark = True
             self.net = self.net.cuda()
-    
-    def letterbox_image(self, image, size):
-        image   = image.convert("RGB")
-        iw, ih  = image.size
-        w, h    = size
-        scale   = min(w/iw, h/ih)
-        nw      = int(iw*scale)
-        nh      = int(ih*scale)
 
-        image       = image.resize((nw,nh), Image.BICUBIC)
-        new_image   = Image.new('RGB', size, (128,128,128))
-        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-        if self.input_shape[-1]==1:
+    def letterbox_image(self, image, size):
+        image = image.convert("RGB")
+        iw, ih = image.size
+        w, h = size
+        scale = min(w / iw, h / ih)
+        nw = int(iw * scale)
+        nh = int(ih * scale)
+
+        image = image.resize((nw, nh), Image.BICUBIC)
+        new_image = Image.new('RGB', size, (128, 128, 128))
+        new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))
+        if self.input_shape[-1] == 1:
             new_image = new_image.convert("L")
         return new_image
-        
-    #---------------------------------------------------#
+
+    # ---------------------------------------------------#
     #   检测图片
-    #---------------------------------------------------#
+    # ---------------------------------------------------#
     def detect_image(self, image_1, image_2):
-        #---------------------------------------------------#
+        # ---------------------------------------------------#
         #   对输入图像进行不失真的resize
-        #---------------------------------------------------#
-        image_1 = self.letterbox_image(image_1,[self.input_shape[1],self.input_shape[0]])
-        image_2 = self.letterbox_image(image_2,[self.input_shape[1],self.input_shape[0]])
-        
-        #---------------------------------------------------#
+        # ---------------------------------------------------#
+        image_1 = self.letterbox_image(image_1, [self.input_shape[1], self.input_shape[0]])
+        image_2 = self.letterbox_image(image_2, [self.input_shape[1], self.input_shape[0]])
+
+        # ---------------------------------------------------#
         #   对输入图像进行归一化
-        #---------------------------------------------------#
+        # ---------------------------------------------------#
         photo_1 = np.asarray(image_1).astype(np.float64) / 255
         photo_2 = np.asarray(image_2).astype(np.float64) / 255
 
-        if self.input_shape[-1]==1:
+        if self.input_shape[-1] == 1:
             photo_1 = np.expand_dims(photo_1, -1)
             photo_2 = np.expand_dims(photo_2, -1)
 
         with torch.no_grad():
-            #---------------------------------------------------#
+            # ---------------------------------------------------#
             #   添加上batch维度，才可以放入网络中预测
-            #---------------------------------------------------#
+            # ---------------------------------------------------#
             photo_1 = torch.from_numpy(np.expand_dims(np.transpose(photo_1, (2, 0, 1)), 0)).type(torch.FloatTensor)
             photo_2 = torch.from_numpy(np.expand_dims(np.transpose(photo_2, (2, 0, 1)), 0)).type(torch.FloatTensor)
-            
+
             if self.cuda:
                 photo_1 = photo_1.cuda()
                 photo_2 = photo_2.cuda()
-                
-            #---------------------------------------------------#
+
+            # ---------------------------------------------------#
             #   获得预测结果，output输出为概率
-            #---------------------------------------------------#
+            # ---------------------------------------------------#
             output = self.net([photo_1, photo_2])[0]
             output = torch.nn.Sigmoid()(output)
 
-        plt.subplot(1, 2, 1)
-        plt.imshow(np.array(image_1))
-
-        plt.subplot(1, 2, 2)
-        plt.imshow(np.array(image_2))
-        plt.text(-12, -12, 'Similarity:%.3f' % output, ha='center', va= 'bottom',fontsize=11)
-        plt.show()
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(np.array(image_1))
+        #
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(np.array(image_2))
+        # plt.text(-12, -12, 'Similarity:%.3f' % output, ha='center', va='bottom', fontsize=11)
+        # plt.show()
         return output
