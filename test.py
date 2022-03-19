d9b6356098a4423c47e0e11c46e54047f16277df
Elwin <abc602456615@163.com>
2022-03-19 10:08:39 +0000

    update

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from sklearn.metrics import ConfusionMatrixDisplay, classification_report
 from siamese import Siamese
 
 test_set = Path('./testdata')
-model_path = Path('./logs_cpy')
+model_path = Path('./logs')
 
 tests = []
 for animal_name in test_set.glob('*'):
@@ -16,19 +16,23 @@ for animal_name in test_set.glob('*'):
     for img_path in animal_name.glob('*'):
         tests.append((img_path.absolute(), animal_name.name))
 
+# TODO: Make the testset balanced
+num_of_true = 0
 atest = []
 # Construct test sets and ground truth
 for i in range(len(tests)):
     for j in range(i + 1, len(tests)):
         path1, animal1 = tests[i]
         path2, animal2 = tests[j]
+        if animal2 == animal1:
+            num_of_true += 1
         atest.append((path1, path2, animal1 == animal2))  # Ground Truth
-
+# print(num_of_true)
 
 def evaluation(predict, truth):
     # Confusion Matrix
     disp = ConfusionMatrixDisplay.from_predictions(truth, predict)
-    disp.figure_.suptitle("Confusion Matrix")
+    disp.figure_.suptitle("Confusion Matrix for {}".format(i))
     print(f"Confusion matrix:\n{disp.confusion_matrix}")
     plt.show()
     # Other evaluations
@@ -41,6 +45,8 @@ def evaluation(predict, truth):
 
 
 def main(model_pth):
+    global num_of_true
+    num_of_true_dp = num_of_true
     model = Siamese(model_path=model_pth)
     Y = []
     prediction = []
@@ -50,23 +56,30 @@ def main(model_pth):
         # if process == 1000:
         #     break
         p1, p2, res = tup
+        if not res:
+            if num_of_true_dp != 0:
+                num_of_true_dp = num_of_true_dp - 1
+            else:
+                continue
         img_1 = Image.open(p1)
         img_2 = Image.open(p2)
         Y.append(res)
         result = model.detect_image(img_1, img_2)
         prediction.append(True if result > 0.5 else False)
         process += 1
-        print(f"Processing...({process}/{total})")
+        print(f"Processing...({process}/{num_of_true * 2})")
     # print(Y)
     # print(prediction)
     return evaluation(prediction, Y)
 
 
 evaluation_feedback = {}
-for i in model_path.glob('*'):
+for i in model_path.glob('*.pth'):
     print(f'loading model {i}...')
     evaluation_feedback[i.name] = main(i)
 
 print(evaluation_feedback)
 with open('output.json', 'w+') as f:
     json.dump(evaluation_feedback, f, indent=4)
+
+
